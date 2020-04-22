fde8140041be6c5df7b574183206f68ac1e1aac2
Pavel Belevich <pbelevich@fb.com>
2020-04-21 20:51:47 -0700

    Prerequisites for CSPRNG
    ghstack-source-id: 574f18122f6200274dccc6289b47d0513e7cb563
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/36631

--- aten/src/ATen/core/DistributionsHelper.h
+++ aten/src/ATen/core/DistributionsHelper.h
@@ -10,6 +10,7 @@
 
 #include <ATen/core/Array.h>
 #include <c10/util/Half.h>
+#include <c10/util/BFloat16.h>
 #include <c10/util/Optional.h>
 #include <c10/macros/Macros.h>
 
@@ -35,21 +36,6 @@
 
 namespace at {
 
-// Using VectorType in Box-muller derived distributions to avoid
-// code duplication
-template <typename T>
-struct VectorType {  };
-
-#if defined(__CUDACC__) || defined(__HIPCC__)
-template <> struct VectorType<half> { using type = at::detail::Array<float, 2>; };
-#endif
-template <> struct VectorType<Half> { using type = at::detail::Array<float, 2>; };
-template <> struct VectorType<float> { using type = at::detail::Array<float, 2>; };
-template <> struct VectorType<double> { using type = at::detail::Array<double, 2>; };
-
-template <typename T>
-using vect_type = typename VectorType<T>::type;
-
 // Using DistAccumType in accumulate types for distributions.
 // Note: Ideally we'd be using ATen/AccumulateType.h but looks
 // like the there is some inconsistency in how accumulate types
@@ -68,37 +54,139 @@ template <> struct DistAccumType<double> { using type = double; };
 template <typename T>
 using dist_acctype = typename DistAccumType<T>::type;
 
-// Constants for uniform distribution
-// doubles have 52 bits of mantissa (fractional part)
-constexpr uint64_t DOUBLE_MASK = (1ULL << 53) - 1;
-constexpr double DOUBLE_DIVISOR = 1.0 / (1ULL << 53);
+/**
+ * Samples a discrete uniform distribution in the range [base, base+range) of type T
+ * This is a transformation function for
+ * https://pytorch.org/docs/stable/tensors.html?highlight=random#torch.Tensor.random_
+ * when both `from` and `to` are specified.
+ */
+template <typename T>
+struct uniform_int_from_to_distribution {
 
-// floats have 23 bits of mantissa (fractional part)
-constexpr uint32_t FLOAT_MASK = (1 << 24) - 1;
-constexpr float FLOAT_DIVISOR = 1.0f / (1 << 24);
+  C10_HOST_DEVICE inline uniform_int_from_to_distribution(uint64_t range_, int64_t base_) {
+    range = range_;
+    base = base_;
+  }
+
+  template <typename V,
+            typename std::enable_if<std::is_integral<V>::value, int>::type = 0>
+  C10_HOST_DEVICE inline T operator()(V val) {
+    return static_cast<T>(static_cast<int64_t>((val % range) + base));
+  }
+
+  template <typename RNG,
+            typename std::enable_if<(!std::is_fundamental<RNG>::value), int>::type = 0>
+  C10_HOST_DEVICE inline T operator()(RNG generator) {
+    if ((
+      std::is_same<T, int64_t>::value ||
+      std::is_same<T, double>::value ||
+      std::is_same<T, float>::value ||
+      std::is_same<T, at::BFloat16>::value) && range >= 1ULL << 32)
+    {
+      return operator()(generator->random64());
+    } else {
+      return operator()(generator->random());
+    }
+  }
+
+  private:
+    uint64_t range;
+    int64_t base;
+};
 
 /**
- * Samples a uniform distribution in the range [0,1) of type T
+ * Samples a discrete uniform distribution in the range [min_value(int64_t), max_value(int64_t)]
+ * This is a transformation function for
+ * https://pytorch.org/docs/stable/tensors.html?highlight=random#torch.Tensor.random_
+ * when `from=min_value(int64_t)` and to=None
+ */
+template <typename T>
+struct uniform_int_full_range_distribution {
+
+  template <typename V,
+            typename std::enable_if<std::is_integral<V>::value, int>::type = 0>
+  C10_HOST_DEVICE inline T operator()(V val) {
+    return static_cast<T>(static_cast<int64_t>(val));
+  }
+
+  template <typename RNG,
+            typename std::enable_if<(!std::is_fundamental<RNG>::value), int>::type = 0>
+  C10_HOST_DEVICE inline T operator()(RNG generator) {
+    return operator()(generator->random64());
+  }
+
+};
+
+/**
+ * Samples a discrete uniform distribution in the range [0, max_value(T)] for integral types
+ * and [0, 2^mantissa] for floating point types. This is a transformation function for
+ * https://pytorch.org/docs/stable/tensors.html?highlight=random#torch.Tensor.random_
+ * when used without specifing `from` and `to`.
+ */
+template <typename T>
+struct uniform_int_distribution {
+
+  template <typename V,
+            typename std::enable_if<std::is_integral<V>::value, int>::type = 0>
+  C10_HOST_DEVICE inline T operator()(V val) {
+    if (std::is_same<T, bool>::value) {
+      return static_cast<bool>(val & 1);
+    } else if (std::is_same<T, double>::value) {
+      return static_cast<T>(val % static_cast<uint64_t>((1ULL << std::numeric_limits<T>::digits) + 1));
+    } else if (std::is_same<T, int64_t>::value) {
+      return static_cast<T>(val % (static_cast<uint64_t>(std::numeric_limits<T>::max()) + 1));
+    } else if (std::is_floating_point<T>::value || std::is_same<T, at::Half>::value || std::is_same<T, at::BFloat16>::value) {
+      return static_cast<T>(val % static_cast<uint64_t>((1ULL << std::numeric_limits<T>::digits) + 1));
+    } else if (std::is_integral<T>::value) {
+      return static_cast<T>(val % (static_cast<uint64_t>(std::numeric_limits<T>::max()) + 1));
+    } else {
+      assert(false);
+      return 0;
+    }
+  }
+
+  template <typename RNG,
+            typename std::enable_if<(!std::is_fundamental<RNG>::value), int>::type = 0>
+  C10_HOST_DEVICE inline T operator()(RNG generator) {
+    if (std::is_same<T, double>::value || std::is_same<T, int64_t>::value) {
+      return operator()(generator->random64());
+    } else {
+      return operator()(generator->random());
+    }
+  }
+
+};
+
+/**
+ * Samples a uniform distribution in the range [a, b) of type T
  */
 template <typename T>
 struct uniform_real_distribution {
 
-  inline uniform_real_distribution(T a_in, T b_in) {
-    TORCH_CHECK(a_in <= b_in);
-    TORCH_CHECK(b_in-a_in <= std::numeric_limits<T>::max());
+  C10_HOST_DEVICE inline uniform_real_distribution(T a_in, T b_in) {
+    TORCH_CHECK_IF_NOT_ON_CUDA(a_in <= b_in);
+    TORCH_CHECK_IF_NOT_ON_CUDA(b_in-a_in <= std::numeric_limits<T>::max());
     a = a_in;
     b = b_in;
   }
 
-  template <typename RNG>
-  inline dist_acctype<T> operator()(RNG* generator){
-    dist_acctype<T> x;
-    if(std::is_same<T, double>::value) {
-      x = (generator->random64() & DOUBLE_MASK) * DOUBLE_DIVISOR;
-    } else {
-      x = (generator->random() & FLOAT_MASK) * FLOAT_DIVISOR;
-    }
+  template <typename V,
+            typename std::enable_if<std::is_integral<V>::value, int>::type = 0>
+  C10_HOST_DEVICE inline T operator()(V val) {
+    constexpr auto MASK = static_cast<V>((static_cast<uint64_t>(1) << std::numeric_limits<T>::digits) - 1);
+    constexpr auto DIVISOR = static_cast<T>(1) / (static_cast<uint64_t>(1) << std::numeric_limits<T>::digits);
+    dist_acctype<T> x = (val & MASK) * DIVISOR;
     return (x * (b - a) + a);
+  }
+
+  template <typename RNG,
+            typename std::enable_if<(!std::is_fundamental<RNG>::value), int>::type = 0>
+  C10_HOST_DEVICE inline dist_acctype<T> operator()(RNG generator){
+    if(std::is_same<T, double>::value) {
+      return operator()(generator->random64());
+    } else {
+      return operator()(generator->random());
+    }
   }
 
   private:
@@ -116,14 +204,15 @@ template <typename T>
 struct normal_distribution {
 
   inline normal_distribution(T mean_in, T stdv_in) {
-    TORCH_CHECK(stdv_in > 0);
+    TORCH_CHECK_IF_NOT_ON_CUDA(stdv_in > 0);
     mean = mean_in;
     stdv = stdv_in;
   }
 
   template <typename RNG>
-  inline dist_acctype<T> operator()(RNG* generator){
+  inline dist_acctype<T> operator()(RNG generator){
     dist_acctype<T> ret;
+#if !defined(__CUDACC__) && !defined(__HIPCC__)
     // return cached values if available
     if (std::is_same<T, double>::value) {
       if (generator->next_double_normal_sample()) {
@@ -140,12 +229,14 @@ struct normal_distribution {
         return ret;
       }
     }
+#endif
     // otherwise generate new normal values
     uniform_real_distribution<T> uniform(0.0, 1.0);
     const dist_acctype<T> u1 = uniform(generator);
     const dist_acctype<T> u2 = uniform(generator);
     const dist_acctype<T> r = ::sqrt(static_cast<T>(-2.0) * ::log(static_cast<T>(1.0)-u2));
     const dist_acctype<T> theta = static_cast<T>(2.0) * static_cast<T>(M_PI) * u1;
+#if !defined(__CUDACC__) && !defined(__HIPCC__)
     if (std::is_same<T, double>::value) {
       dist_acctype<double> cache = r * ::sin(theta);
       generator->set_next_double_normal_sample(c10::optional<double>(cache));
@@ -153,6 +244,7 @@ struct normal_distribution {
       dist_acctype<float> cache = r * ::sin(theta);
       generator->set_next_float_normal_sample(c10::optional<float>(cache));
     }
+#endif
     ret = r * ::cos(theta) * stdv + mean;
     return ret;
   }
@@ -174,7 +266,7 @@ struct bernoulli_distribution {
   }
 
   template <typename RNG>
-  inline int operator()(RNG* generator) {
+  inline int operator()(RNG generator) {
     uniform_real_distribution<T> uniform(0.0, 1.0);
     return uniform(generator) < p;
   }
@@ -195,7 +287,7 @@ struct geometric_distribution {
   }
 
   template <typename RNG>
-  inline int operator()(RNG* generator) {
+  inline int operator()(RNG generator) {
     uniform_real_distribution<T> uniform(0.0, 1.0);
     dist_acctype<T> sample = uniform(generator);
     return static_cast<int>(::log(static_cast<T>(1.0)-sample) / ::log(p)) + 1;
@@ -216,7 +308,7 @@ struct exponential_distribution {
   }
 
   template <typename RNG>
-  __ubsan_ignore_float_divide_by_zero__ inline T operator()(RNG* generator) {
+  __ubsan_ignore_float_divide_by_zero__ inline T operator()(RNG generator) {
     uniform_real_distribution<T> uniform(0.0, 1.0);
     dist_acctype<T> sample = uniform(generator);
     return static_cast<T>(-1.0) / lambda * ::log(static_cast<T>(1.0)-sample);
@@ -238,7 +330,7 @@ struct cauchy_distribution {
   }
 
   template <typename RNG>
-  inline T operator()(RNG* generator) {
+  inline T operator()(RNG generator) {
     uniform_real_distribution<T> uniform(0.0, 1.0);
     return median + sigma * ::tan(static_cast<T>(M_PI) * (uniform(generator)-static_cast<T>(0.5)));
   }
@@ -263,7 +355,7 @@ struct lognormal_distribution {
   }
 
   template<typename RNG>
-  inline T operator()(RNG* generator){
+  inline T operator()(RNG generator){
     normal_distribution<T> normal(mean, stdv);
     return ::exp(normal(generator));
   }
